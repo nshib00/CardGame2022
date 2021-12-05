b4746d6fb95c00ce25ef086ea021f260c9e1bf72
Юрий Наумов <ynaumov@mail.ru>
2021-12-06 01:45:43 +0300

    Скрипт карты Стена

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public GameObject Army;
+    public GameObject CardPrefab;
+
+    public void CreateWall()
+    {
+        GameObject CurrentWall = transform.GetChild(0).gameObject;
+        if (CurrentWall.GetComponent<CardInfo>().PlayToBattlefield())
+        {            
+            CurrentWall.transform.SetParent(Army.transform);
+            Army.transform.GetChild(Army.transform.childCount - 1).SetAsFirstSibling();
+        }
+        else return;
+        GameObject NewWall = Instantiate(CardPrefab, transform, false);
+        NewWall.GetComponent<CardInfo>().FillCardInfo((Card)(CurrentWall.GetComponent<CardInfo>().SelfCard.Clone()));
+    }
+}
